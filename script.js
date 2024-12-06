5fab6ef9a4854ed03b66bc66a941f793334e38df
Rikiandikaaaaa <132274436+Rikiandikaaaaa@users.noreply.github.com>
2024-12-06 21:36:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+
+let toggler = document.getElementById("toggler")
+let navbar = document.getElementById("navbar")
+let dropdown = document.querySelector(".dropdown-menu")
+let close = document.querySelector(".close")
+
+toggler.addEventListener("click", function () {
+    navbar.style.display = ("flex")
+    close.style.display = ("flex")
+    toggler.style.display = ("none")
+})
+
+close.addEventListener("click", function () {
+    navbar.style.display = ("none")
+    close.style.display = ("none")
+    toggler.style.display = ("block")
+})
+
+dropdown.addEventListener("click", function (e) {
+    if (e.classList = "dropdown-item" && innerWidth < 900) {
+        navbar.style.display = ("none")
+        close.style.display = ("none")
+        toggler.style.display = ("block")
+    }
+})
+
